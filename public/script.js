4074364ac0878158cafe5ad7e9ec9d0828f160f5
EvertonLDS815 <user.name>
2023-02-23 14:07:12 -0300

    branch develop em ação

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 const filterElement = document.getElementById("search-no");
 const cards = document.querySelectorAll(".nada");
+const view = document.querySelector("body");
 
 filterElement.addEventListener("input", filterNameCards);
 
@@ -7,17 +8,25 @@ function filterNameCards() {
     if (filterElement.value !== "") {
         for (let card of cards) {
             let title = card.querySelector(".name-no h3");
-            title = title.textContent.toLowerCase();
-            let filterText = filterElement.value.toLowerCase();
+            title = title.textContent.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+            let filterText = filterElement.value.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
             if (!title.includes(filterText)) {
                 card.style.display = "none";
             } else {
-                card.style.display = "block";
+                if (view.clientWidth > 420) {
+                    card.style.display = "initial";
+                } else {
+                    card.style.display = "flex";
+                }
             }
         }
     } else {
         for (let card of cards) {
-            card.style.display = "block";
+            if (view.clientWidth > 420) {
+                card.style.display = "initial";
+            } else {
+                card.style.display = "flex";
+            }
         }
     }
 }
