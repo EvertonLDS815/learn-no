4c3963e7c61466ce6ceea530acd32c770a26094f
EvertonLDS815 <user.name>
2023-03-03 12:14:46 -0300

    trim

--- public/script.js
+++ public/script.js
@@ -10,8 +10,8 @@ function filterNameCards() {
     if (filterElement.value !== "") {
         for (let card of cards) {
             let title = card.querySelector(".name-no h3");
-            title = title.textContent.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-            let filterText = filterElement.value.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+            title = title.textContent.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").trim();
+            let filterText = filterElement.value.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").trim();
             if (!title.includes(filterText)) {
                 card.style.display = "none";
             } else {
