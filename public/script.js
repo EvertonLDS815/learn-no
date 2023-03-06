8c305b1216e3c5878dbed8b20eb76d51d4fb05e5
EvertonLDS815 <user.name>
2023-03-05 22:07:03 -0300

    type and nivel

--- public/script.js
+++ public/script.js
@@ -7,10 +7,29 @@ function filterNameCards() {
     if (filterElement.value !== "") {
         for (let card of cards) {
             let title = card.querySelector(".name-no h3");
+            let type = card.querySelector(".name-no .type");
+            let nivel = card.querySelector(".name-no .secondtype");
             title = title.textContent.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").trim();
+            type = type.textContent.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").trim();
+            nivel = nivel.textContent.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").trim();
             let filterText = filterElement.value.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").trim();
             if (!title.includes(filterText)) {
                 card.style.display = "none";
+                if (!title.includes(filterText)) {
+                    card.style.display = "none";
+                } else {
+                    card.style.display = "block";
+                }
+                if (!nivel.includes(filterText)) {
+                    card.style.display = "none";
+                    if (!type.includes(filterText)) {
+                        card.style.display = "none";
+                    } else {
+                        card.style.display = "block";
+                    }
+                } else {
+                    card.style.display = "block";
+                }
             } else {
                 card.style.display = "block";
             }
